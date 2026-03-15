8d044ecafb306fcd1b5d144262b03c23bb31e8ce
theeyewitnesseshiddentruth-svg <the.eye.witnesses.hidden.truth@gmail.com>
2026-03-15 08:44:32 +0200

    Update uploader.py

--- scripts/uploader.py
+++ scripts/uploader.py
@@ -1,1 +1,20 @@
+from googleapiclient.discovery import build
+import os
 
+def upload_video(file, title, description="Shorts Video", tags=["shorts"]):
+    youtube = build("youtube", "v3", developerKey=os.getenv("YOUTUBE_KEY"))
+    
+    request = youtube.videos().insert(
+        part="snippet,status",
+        body={
+            "snippet": {
+                "title": title,
+                "description": description,
+                "tags": tags,
+                "categoryId": "22"
+            },
+            "status": {"privacyStatus": "public"}
+        },
+        media_body=file
+    )
+    request.execute()
